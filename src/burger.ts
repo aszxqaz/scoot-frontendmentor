caab0e2d882c426e2bbcc36af1989db4ae02f744
aszxqaz <maxim.butenko92.1@gmail.com>
2022-09-07 11:13:32 +0300

    stable

--- src/burger.ts
+++ src/burger.ts
@@ -1,28 +1,26 @@
 const $burger = document.getElementById('burger');
 const $close = document.getElementById('burger-close');
-const $nav = document.getElementById('nav')
+const $nav = document.getElementById('nav');
 
 export function burgerInit() {
   const toggle = () => {
-    $burger?.classList.toggle('hidden')
-    $close?.classList.toggle('hidden')
-    $nav?.classList.toggle('opened')
-  }
-  console.dir($burger)
-  $close?.addEventListener('click', toggle)
-  $burger?.addEventListener('click', toggle)
+    $burger?.classList.toggle('hidden');
+    $close?.classList.toggle('hidden');
+    $nav?.classList.toggle('opened');
+  };
+  console.dir($burger);
+  $close?.addEventListener('click', () => {
+    toggle();
+    document.body.classList.toggle('stop-scroll');
+  });
+  $burger?.addEventListener('click', () => {
+    toggle();
+    document.body.classList.toggle('stop-scroll');
+  });
   $nav?.addEventListener('click', (e) => {
-    if (!(e.target as HTMLElement).closest('a') && (e.target as HTMLElement).closest('ul')) return
-    // const sectionName = (e.target as HTMLElement).getAttribute('href')?.slice(1)
-    // if (sectionName) {
-    //   for(const el of Array.from(document.querySelectorAll(`[data-page]`))) {
-    //     if (el.getAttribute('data-page') === sectionName) {
-    //       el.classList.remove('none')
-    //       continue
-    //     }
-    //     el.classList.add('none')
-    //   }
-    // }
-    toggle()
-  })
+    if (!(e.target as HTMLElement).closest('a') && (e.target as HTMLElement).closest('ul')) return;
+    toggle();
+    document.body.classList.remove('stop-scroll');
+
+  });
 }
